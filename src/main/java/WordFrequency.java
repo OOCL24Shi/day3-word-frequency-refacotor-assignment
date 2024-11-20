a64f762ce39c9372c6a8a2b92af2b3621c194d38
VSHI <36368406@QQ.COM>
2024-11-20 20:44:04 +0800

    Refactor: renamed value to word

--- src/main/java/WordFrequency.java
+++ src/main/java/WordFrequency.java
@@ -1,15 +1,15 @@
 public class WordFrequency {
-    private String value;
+    private String word;
     private int count;
 
     public WordFrequency(String word, int count){
-        this.value =word;
+        this.word =word;
         this.count =count;
     }
 
 
-    public String getValue() {
-        return this.value;
+    public String getWord() {
+        return this.word;
     }
 
     public int getWordCount() {
